e863b10e2eaa94bb805ea51575eca8387abcd14b
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-04 16:21:45 +0700

    Add all file

---
+++ src/com/calculatordays/Main.java
@@ -0,0 +1,46 @@
+package com.calculatordays;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String daysInMonth;
+        System.out.println("Which month that you want to count day?");
+        int month = scanner.nextInt();
+
+        switch (month) {
+            case 2:
+//                System.out.print("The month '2' has 28 or 29 days?");
+                daysInMonth = "28 or 29";
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+//                System.out.print("The month '" + month + "' has 31 days!");
+                daysInMonth = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+//                System.out.print("The month '" + month + "' has 30 days!");
+                daysInMonth = "30";
+                break;
+            default:
+//                System.out.print("Invalid input!");
+                daysInMonth = "";
+        }
+
+        if (daysInMonth != "") {
+            System.out.printf("The month '%d' has %s days!", month, daysInMonth);
+        }else {
+            System.out.print("Invalid input!");
+        }
+    }
+}
